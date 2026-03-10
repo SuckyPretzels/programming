65121286907783811a69e7614b702875f0296469
Pretzels <117748267+SuckyPretzels@users.noreply.github.com>
2026-03-10 17:47:41 -0300

    fiz algumas mudanças nas tarefas da AV1. nem lembro qual foram as mudanças

--- javascript/AV1/AV1 - 1.js
+++ javascript/AV1/AV1 - 1.js
@@ -1,18 +1,20 @@
-let age, contribution
+const prompt = require('prompt-sync')({ sigint: true });
 
-age = Number(prompt("Qual sua idade? "))
+let age, contribution;
+
+age = Number(prompt("Qual sua idade? "));
 
 if (age >= 65) {
     console.log("Você pode se aposentar :)");
 } else if (age < 60) {
-    console.log("Você não pode se aposentar :(")
+    console.log("Você não pode se aposentar :(");
 } else {
-    contribution = Number(prompt("Qual seu tempo de contribuição? "))
+    contribution = Number(prompt("Qual seu tempo de contribuição? "));
 
     if (age >= 60 && contribution >= 30) {
         console.log("Você pode se aposentar :)");
     } else {
-        console.log("Você não pode se aposentar :(")
+        console.log("Você não pode se aposentar :(");
     }
 }
 
--- javascript/AV1/AV1 - 2.js
+++ javascript/AV1/AV1 - 2.js
@@ -1,24 +1,26 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
 let nota1, nota2, nota3, notaParcial, notaFinal, frequencia;
 
 nota1 = Number(prompt("Qual sua nota na AV1? "));
 nota2 = Number(prompt("Qual sua nota na AV2? "));
-notaParcial = (nota1 + nota2) / 2
+notaParcial = (nota1 + nota2) / 2;
 
 
 if (notaParcial >= 4) {
     nota3 = Number(prompt("Qual sua nota na Av3? "));
-    notaFinal = (notaParcial + nota3) / 2
+    notaFinal = (notaParcial + nota3) / 2;
     if (notaFinal >= 5) {
-        frequencia = Number(prompt("Qual sua frequência? (somente o número) "))
-        frequencia = frequencia / 10
+        frequencia = Number(prompt("Qual sua frequência? (somente o número) "));
+        frequencia = frequencia / 10;
         
         if (frequencia >= 0.75) {
-            console.log("Você for aprovado :)")
+            console.log("Você for aprovado :)");
         } else {
-            console.log("Você for reprovado na nota final.")
+            console.log("Você for reprovado na nota final.");
         }
     }
     
 } else {
-    console.log("Você foi reprovado na nota parcial.")
+    console.log("Você foi reprovado na nota parcial.");
 }
