edd1ccd8df1f682a3acf3ceb77afc80622e045e0
Pretzels <117748267+SuckyPretzels@users.noreply.github.com>
2026-03-29 01:01:27 -0300

    basically finished my calculator. this was really cool to make. will be trying to make some improvements in the future

--- javascript/PracticeProjects/Calculator/index.js
+++ javascript/PracticeProjects/Calculator/index.js
@@ -1,1 +1,42 @@
-console.log("Hello, World");
+const display = document.getElementById('display');
+display.value = "0";
+
+let equals = false;
+
+function addToDisplay(s) {
+    if (display.value == "0" && s != ".") {
+        display.value = s;
+    } else if (equals == true && (!isNaN(s) || s == ".")) {
+        display.value = s;
+    } else {
+        display.value += s;
+    }
+    equals = false;
+}
+
+function backspace() {
+    display.value = display.value.slice(0, -1) || "0";
+}
+
+function clearDisplay() {
+    display.value = "0";
+    equals = false;
+}
+
+function calculate() {
+    let input = display.value;
+    let expression = "";
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == "÷") {
+            expression += "/";
+        } else if (input[i] == "×") {
+            expression += "*";
+        } else if (input[i] == "−") {
+            expression += "-";
+        } else {
+            expression += input[i];
+        }
+    }
+    display.value = eval(expression);
+    equals = true;
+}
